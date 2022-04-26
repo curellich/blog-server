158425a1c5d4e9580e9bc1a9861eaf382bd9b2dd
curellich <Curellich@gmail.com>
2022-04-26 16:57:47 -0300

    modifico el puerto de la base de datos

--- database/db.js
+++ database/db.js
@@ -10,8 +10,8 @@ import {Sequelize} from "sequelize";
 const db = new Sequelize('heroku_e567a4ff2c97edd', 'b1cdd098182478', '8f32a79a',
     {
         host: "us-cdbr-east-05.cleardb.net",
-        dialect: "mysql"
-
+        dialect: "mysql",
+        port: "8080"
     });
 
 export default db;
