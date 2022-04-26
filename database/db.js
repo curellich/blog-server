e58831a65d7e4156e7fbe6b759fdd26637261191
curellich <Curellich@gmail.com>
2022-04-26 17:25:42 -0300

    ..

--- database/db.js
+++ database/db.js
@@ -11,7 +11,7 @@ const db = new Sequelize('heroku_e567a4ff2c97edd', 'b1cdd098182478', '8f32a79a',
     {
         host: "us-cdbr-east-05.cleardb.net",
         dialect: "mysql",
-        port: '8080'
+        // port: '8080'
     });
 
 export default db;
